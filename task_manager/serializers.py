21862c0d9877142270b546912104eb36884ab34e
lrsrivera <lr.srivera@mmdc.mcl.edu.ph>
2025-01-26 23:47:21 +0800

    Add files via upload

---
+++ task_manager/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import User, Task
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'is_completed', 'user']
---
+++ task_manager/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('users/', views.get_users, name='get_users'),
+    path('users/create/', views.create_user, name='create_user'),
+    path('tasks/', views.get_tasks, name='get_tasks'),
+    path('tasks/create/', views.create_task, name='create_task'),
+    path('tasks/<int:task_id>/update/', views.update_task, name='update_task'),
+    path('tasks/<int:task_id>/delete/', views.delete_task, name='delete_task'),
+]
