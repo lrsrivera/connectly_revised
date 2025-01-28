afea27d5502f2e185006a4c3ea12db469a1cdc0f
lrsrivera <lr.srivera@mmdc.mcl.edu.ph>
2025-01-29 00:59:30 +0800

    Delete task_manager directory

--- task_manager/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_completed', 'user')  
-    search_fields = ('title', 'user__username') 
-    list_filter = ('is_completed',) 
